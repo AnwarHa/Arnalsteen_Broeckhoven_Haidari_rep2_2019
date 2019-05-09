25bb2404345b36550d7547766b5096ba75a4238e
Kalimath <mathieu_broe@yahoo.com>
2019-05-09 16:37:43 +0200

    dbService initialized in CategoryController

--- SelfAssess/src/controller/CategoryController.java
+++ SelfAssess/src/controller/CategoryController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import testDatabase.DatabaseContext;
+import testDatabase.DatabaseService;
+import testDatabase.SerializableCategoryDatabase;
+
 public class CategoryController {
+    private DatabaseService databaseService;
+
+    public CategoryController() {
+        databaseService = new DatabaseService(new DatabaseContext(new SerializableCategoryDatabase()));
+    }
 }
