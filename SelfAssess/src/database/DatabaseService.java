f79dfafc32edce7855700339463ad84a43b2d281
Anwar <anwar-360@hotmail.com>
2019-05-18 18:55:07 +0200

    getCategoryNames (& zonder duplicates) + initialisatie categorylist

--- SelfAssess/src/database/DatabaseService.java
+++ SelfAssess/src/database/DatabaseService.java
@@ -14,58 +14,67 @@ public class DatabaseService {
     private List<Question> questions;
     private List<Category> categories;
 
-    public DatabaseService(QuestionDatabaseContext q,CategoryDatabaseContext c) {
+    public DatabaseService(QuestionDatabaseContext q, CategoryDatabaseContext c) {
+        categories = new ArrayList<>();
         setCategoryDatabaseContext(c);
         setQuestionDatabaseContext(q);
         this.categoryDatabaseContext.readData();
     }
 
-    private void setQuestionDatabaseContext(QuestionDatabaseContext questionDatabaseContext){
+    private void setQuestionDatabaseContext(QuestionDatabaseContext questionDatabaseContext) {
         this.questionDatabaseContext = questionDatabaseContext;
     }
 
-    private void setCategoryDatabaseContext(CategoryDatabaseContext categoryDatabaseContext){
+    private void setCategoryDatabaseContext(CategoryDatabaseContext categoryDatabaseContext) {
         this.categoryDatabaseContext = categoryDatabaseContext;
     }
 
-    public List<Category> getCategories(){
+    public List<Category> getCategories() {
         return categories;
     }
 
-    public List<Question> getQuestions(){
+    public List<Question> getQuestions() {
         return questions;
     }
 
-    public void setCategories(List<Category> categories){
-        if(categories.isEmpty()){
+    public void setCategories(List<Category> categories) {
+        if (categories.isEmpty()) {
             throw new DatabaseException("can not set categories: list is empty");
-        }else{
+        } else {
             this.categories = categories;
         }
 
     }
 
-    public void setQuestions(List<Question> questions){
-        if(questions.isEmpty()){
+    public void setQuestions(List<Question> questions) {
+        if (questions.isEmpty()) {
             throw new DatabaseException("can not set questions: list is empty");
-        }else{
+        } else {
             this.questions = questions;
         }
 
     }
 
-    public List<String> getCategoryDescriptions(){
+    public List<String> getCategoryDescriptions() {
         List<String> desc = new ArrayList<>();
-        for(Category c : categories){
+        for (Category c : categories) {
             desc.add(c.getDescription());
         }
         return desc;
     }
 
-    public List<String> getQuestionStatements(Question q){
+    public List<String> getCategoryNames() {
+        List<String> names = new ArrayList<>();
+        for (Category c : categories) {
+            names.add(c.getName());
+        }
+        return names;
+    }
+
+    public List<String> getQuestionStatements(Question q) {
         List<String> stat = new ArrayList<>();
-        for(Question c : questions){
-            if(c.equals(q)){
+        for (Question c : questions) {
+            if (c.equals(q)) {
                 stat = c.getAnswers();
                 break;
             }
@@ -73,9 +82,22 @@ public class DatabaseService {
         return stat;
     }
 
+
     public List<Object> getAll() {
         return questionDatabaseContext.readData();
     }
 
 
+    public ArrayList<String> getCategoryNamesWithoutDuplicates() {
+        ArrayList<String> newList = new ArrayList<>();
+        for (String s : getCategoryNames()) {
+            if (!newList.contains(s)) {
+                newList.add(s);
+            }
+        }
+        return newList;
+    }
 }
+
+
+
